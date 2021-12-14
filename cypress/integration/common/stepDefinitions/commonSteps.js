32e6ecb77bb832db06388b68cb6a24ae272afb99
Your Name <you@example.com>
2021-12-14 23:51:45 +0100

    V 01

--- cypress/integration/common/stepDefinitions/commonSteps.js
+++ cypress/integration/common/stepDefinitions/commonSteps.js
@@ -1,7 +1,76 @@
 Given('I open the Google web url', () => {
   cy.visit('https://www.google.com');
-});
-  
+})
 Then('I verify title of web page as {string}', (title) => {
   cy.title().should('include', title);
-});
+})
+
+Given('I open the Facebook web url', () => {
+  cy.visit('https://www.facebook.com');
+})
+Then('I verify title of web page as {string}', (title) => {
+  cy.title().should('include', title);
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify the first name of a pet as {string}', (title) => {
+  cy.get('.card-title').first().should('have.text', title);
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify age of a pet as {string}', (title) => {
+  cy.get('.list-group-item').first().should('include.text', title);
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify header as {string}', (title) => {
+  cy.get('h1').first().should('include.text', title);
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify button as {string}', (title) => {
+  cy.get('.nav-link').should('include.text', title);
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify a name of a pet as {string}', (title) => {
+  cy.get('.card-title').should('include.text', title);
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify if any pet is important', () => {
+  cy.get('.badge').should('include.text', 'Important');
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify if any pet is new', () => {
+  cy.get('.badge').should('include.text', 'New');
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify if any pet is negative', () => {
+  cy.get('.age').should('include.text', '-');
+})
+
+Given('I open the Pet database web url', () => {
+  cy.visit('http://the-pettest-store.herokuapp.com/pet');
+})
+Then('I verify if {string} is on the page', (title) => {
+  cy.get('body').should('include.text', title);
+})
--- cypress/integration/features/UI/bdd-demo.feature
+++ cypress/integration/features/UI/bdd-demo.feature
@@ -1,5 +1,46 @@
-Feature: Navigating to Google.com and verifying title
+Feature: Navigating to different websites and verifying title
   @test
   Scenario: Perform Navigation
     Given I open the Google web url
     Then I verify title of web page as 'Google'
+
+
+  Scenario: Perform Navigation
+    Given I open the Facebook web url
+    Then I verify title of web page as 'Facebook'
+
+Scenario: Perform Navigation and verify header
+    Given I open the Pet database web url
+    Then I verify header as 'Pet Store'
+
+  Scenario: Perform Navigation and verify pet name
+    Given I open the Pet database web url
+    Then I verify the first name of a pet as 'Bauer'
+
+  Scenario: Perform Navigation and verify pet age
+    Given I open the Pet database web url
+    Then I verify age of a pet as '1'
+
+  Scenario: Perform Navigation and verify button
+    Given I open the Pet database web url
+    Then I verify button as 'Create a pet'
+
+  Scenario: Perform Navigation and verify pet name
+    Given I open the Pet database web url
+    Then I verify a name of a pet as 'Bobo'
+
+  Scenario: Perform Navigation and verify pet status
+    Given I open the Pet database web url
+    Then I verify if any pet is important
+
+  Scenario: Perform Navigation and verify pet status
+    Given I open the Pet database web url
+    Then I verify if any pet is new
+
+  Scenario: Perform Navigation and verify pet age as negative
+    Given I open the Pet database web url
+    Then I verify if any pet is negative
+
+  Scenario: Perform Navigation and verify a word on the page
+    Given I open the Pet database web url
+    Then I verify if 'Detail' is on the page
